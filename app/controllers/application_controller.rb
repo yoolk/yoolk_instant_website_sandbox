c09df926cc46f0781b80c9310c7f2df0e0beb003
radin-reth <radin.reth@gmail.com>
2014-09-02 17:18:49 +0700

    [#77992622] add default url options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   theme :theme_resolver
+
+  def default_url_options
+    { theme: request.parameters["theme"], locale: params[:locale] }
+  end
 
   private
 
