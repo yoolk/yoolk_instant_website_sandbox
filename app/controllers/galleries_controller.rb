98f2d6a45c85f4a33567e1f4d4bed5f24d5c1ae1
radin-reth <radin.reth@gmail.com>
2014-09-08 09:13:13 +0700

    [#77992622] add gallery show

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -3,6 +3,6 @@ class GalleriesController < ApplicationController
   end
 
   def show
-    @gallery = @listing.image_galleries.find(params[:id].to_i)
+    @image_gallery = @listing.image_galleries.select{|album| album.id == params[:id].to_i }[0]
   end
 end
