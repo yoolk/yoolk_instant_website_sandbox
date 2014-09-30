b4266f5afb06731cf0c6066a58d793f9220b42bd
radin-reth <radin.reth@gmail.com>
2014-09-30 12:06:38 +0700

    [#77992622] :100: update form tag, set validation

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -4,23 +4,23 @@ class ReservationController < ApplicationController
   end
 
   def create
-    if true#validate_params(reservation_params)
-      @message = 'success'
-    else
-      @message = 'false'#['false', 1, 2, 3]
-    end
-
-    redirect_to reservation_path, notice: @message
+    @message = validate_params(reservation_params)
+    redirect_to reservation_path, notice: @message#I18n.t('sample.welcome')
   end
 
   private
   def validate_params(params)
-    # EMAIL_REG = /[a-zA-Z0-9._%-]+@(?:[a-zA-Z0-9-]+\.)+(com|net|org|info|biz|me|edu|gov)/i
+    email_reg = /[a-zA-Z0-9._%-]+@(?:[a-zA-Z0-9-]+\.)+(com|net|org|info|biz|me|edu|gov)/i
 
-    validates           :sender, :body, presence: {message: I18n.t(:"messages.invalid")}
-    validates_format_of :sender,    with: EMAIL_REG, message: I18n.t(:"messages.invalid_email")
+    ary = []
+    ary.push 'Email is not provided'           unless params[:email].present?
+    ary.push 'Email validation fail'           unless params[:email].match(email_reg)
+    ary.push 'Special request is not provided' unless params[:request].present?
+    ary.push 'Validation passed'               if ary.empty?
 
+    ary
   end
+
   def reservation_params
     params.require(:reservation).permit(:check_in, :check_out, :guests, :room_types, :name, :phone, :email, :request)
   end
