738cdfef95bd293f2a4edcc27bc29bc659ca91e9
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-01 17:37:51 +0700

    update controller

--- app/controllers/menu/categories_controller.rb
+++ app/controllers/menu/categories_controller.rb
@@ -1,5 +1,5 @@
 class Menu::CategoriesController < ApplicationController
   def show
-    @menu = @listing.food_categories.select{|category| category.id == params[:category_id].to_i }[0]
+    @food_category = @listing.food_categories.find { |category| category.id == params[:category_id].to_i }
   end
 end
--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,8 +1,4 @@
 class MenuController < ApplicationController
   def index
   end
-
-  def show
-    @food = @listing.foods.select{|food| food.id == params[:id].to_i }[0]
-  end
 end
