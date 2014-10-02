3dcad201668982e139ff481f40e95e103c44b246
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-02 16:40:31 +0700

    integrate feedback form

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,4 +1,22 @@
 class FeedbackController < ApplicationController
+
   def index
+    @feedback = Yoolk::Form::Feedback.new
   end
+
+  def create
+    @feedback = Yoolk::Form::Feedback.new(feedback_params)
+
+    if @feedback.valid?
+      redirect_to feedback_index_path, notice: 'Thank you for submitting your feedback.'
+    else
+      render :index
+    end
+  end
+
+  private
+
+    def feedback_params
+      params.require(:feedback).permit(:name, :sender, :body)
+    end
 end
