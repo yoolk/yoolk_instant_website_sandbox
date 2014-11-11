eaf5356ddde8a7ed1b3b11ce6a210df1daff685c
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-11-11 16:39:18 +0700

    add rake script to validate themes

---
+++ spec/theme_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+require 'rake'
+Rails.application.load_tasks
+
+ThemesOnRails.all.each do |theme|
+  theme_directory   = "app/themes/#{theme}"
+  views_directory   = "#{theme_directory}/views"
+  locales_directory = "#{theme_directory}/locales"
+
+  context "#{theme}: view files" do
+    it 'contains only liquid templates' do
+      files             = Dir["#{views_directory}/**/*"].reject { |file| File.directory?(file) }
+      non_liquid_files  = files.count { |file| !file.end_with?('.liquid') }
+
+      expect(non_liquid_files).to be_zero
+    end
+  end
+
+  context "#{theme}: locales files" do
+    Dir["#{locales_directory}/*"].each do |yaml_file|
+      it 'parses successfully' do
+        expect { YAML.load_file(yaml_file) }.to_not raise_error
+      end
+
+      it 'contains valid keys' do
+        locale = YAML.load_file(yaml_file)
+
+        expect(locale.keys.length).to eq(1)
+        expect(locale[locale.keys[0]].keys).to eq([theme])
+      end
+    end
+  end
+end
+
+context 'asset precompilation' do
+  after(:each) { FileUtils.rm_rf('public/assets') }
+
+  it 'passes the precompile test' do
+    expect {
+      ENV['RAILS_ENV'] = 'production'
+
+      Rake::Task['assets:precompile'].invoke
+
+      ENV['RAILS_ENV'] = 'test'
+    }.to_not raise_error
+  end
+end
