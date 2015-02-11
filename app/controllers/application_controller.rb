a7b1e612ad6e12cecf47967ad3f623c3f267ef39
radin-reth <radin.reth@gmail.com>
2015-02-11 10:50:49 +0700

    add liquid seo title view assign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -101,6 +101,6 @@ class ApplicationController < ActionController::Base
     end
 
     def liquid_assigns
-      view_assigns.merge('content_for_header' => content_for_header)
+      view_assigns.merge('content_for_header' => content_for_header, 'seo_title' => seo.title)
     end
 end
