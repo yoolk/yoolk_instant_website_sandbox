99bd49753373fa66c6fe4377556c3d3ce0940083
Chamnap Chhorn <chamnapchhorn@gmail.com>
2015-03-10 15:38:05 +0700

    add spec on .css.scss and content_for_layout

--- spec/theme_spec.rb
+++ spec/theme_spec.rb
@@ -7,6 +7,7 @@ ThemesOnRails.all.each do |theme|
   views_directory   = "#{theme_directory}/views"
   locales_directory = "#{theme_directory}/locales"
   assets_directory  = "#{theme_directory}/assets"
+  theme_layout_file = "#{views_directory}/layouts/#{theme}.liquid"
   available_locales = %w(af ar az bg bn bs ca cs cy da de de-AT de-CH el en en-AU en-CA en-GB en-IE en-IN en-NZ en-US en-ZA eo es es-419 es-AR es-CL es-CO es-CR es-EC es-MX es-PA es-PE es-US es-VE et eu fa fi fr fr-CA fr-CH gl he hi hi-IN hr hu id is it it-CH ja km kn ko lo lt lv mk mn ms nb ne nl nn or pl pt pt-BR rm ro ru sk sl sr sv sw ta th tl tr uk ur uz vi wo zh-CN zh-HK zh-TW zh-YUE)
 
   context "Theme #{theme}: view files" do
@@ -25,6 +26,12 @@ ThemesOnRails.all.each do |theme|
         expect(validator.valid?).to be_truthy, "template: #{file}, errors: #{validator.errors}"
       end
     end
+
+    it 'should invoke {{content_for_header}} inside layout file' do
+      content = File.read(theme_layout_file)
+
+      expect(content).to match(/{{\s*content_for_header\s*}}/), "layout file: `#{theme_layout_file}` should include `{{ content_for_header }}` inside <head></head> section."
+    end
   end
 
   context "Theme #{theme}: directory structure" do
@@ -42,6 +49,12 @@ ThemesOnRails.all.each do |theme|
       css_directory = assets_directory + "/stylesheets/#{theme}/all*.scss"
 
       expect(Dir.glob(css_directory)).to be_present, "there must be `all.scss` inside app/themes/#{theme}/stylesheets/#{theme}/"
+    end
+
+    it 'should not contains *.css.scss' do
+      sass_files = assets_directory + "/stylesheets/#{theme}/*.css.scss"
+
+      expect(Dir.glob(sass_files)).to be_empty, "Extra .css in SCSS file is unnecessary. Rename these files to end with `.scss` only."
     end
 
     it 'contains views directory' do
