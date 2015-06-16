fa50d9d6b9c01c34419e48e0eeab7db9de3dd386
radin-reth <radin.reth@gmail.com>
2015-06-16 08:41:33 +0700

    update product and food controller

--- app/controllers/menu/foods_controller.rb
+++ app/controllers/menu/foods_controller.rb
@@ -1,5 +1,12 @@
 class Menu::FoodsController < ApplicationController
   def show
     @food = @listing.foods.find { |food| food.id == params[:id].to_i }
+    if params[:category_id]
+      @food_category = @listing.food_categories.find { |category| category.id == params[:category_id].to_i }
+
+      if @food_category.nil?
+        redirect_to menu_food_path(@food)
+      end
+    end
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,13 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = @listing.products.find { |product| product.id == params[:id].to_i }
+    @product ||= @listing.products.find { |product| product.id == params[:id].to_i }
+    if params[:category_id]
+      @product_category = @product.product_categories.find { |category| category.id == params[:category_id].to_i }
+
+      if @product_category.nil?
+        redirect_to product_path(@product)
+      end
+    end
   end
 end
